98c2651dc033deb26ec8adf8295ae18bd097b38b
Sagar Shinde <sagar@fice.in>
2021-04-09 22:40:31 +0530

    Added bmi_cal.py and updated main.py
    Added functionality of bmi calculation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import input
+import bmi_cal
 
 w = input.weight
 
@@ -7,3 +8,7 @@ h = input.height
 print("Weight in kg is",w)
 
 print("Height in meters is",h)
+
+Body_mass_index = bmi(w,h)
+
+print("Body mass index of the person:",Body_mass_index)
